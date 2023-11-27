aa689f5813d14abc664fbf17b0fe55bef54e506f
Loverboizz <binhnt.22git@vku.udn.vn>
2023-11-27 20:31:55 +0700

    #59

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -16,6 +16,17 @@ class UserRedux extends Component {
             roleArr:[],
             previewImgURL: '',
             isOpen: false,
+
+            email: '',
+            password: '',
+            firstName: '',
+            lastName: '',
+            phoneNumber: '',
+            address: '',
+            gender: '',
+            position: '',
+            role: '',
+            avatar: '',
         }
     }
 
@@ -30,18 +41,24 @@ async componentDidMount() {
     
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (prevProps.genderRedux !== this.props.genderRedux) {
+            let arrGenders = this.props.genderRedux
             this.setState({
-                genderArr: this.props.genderRedux
+                genderArr: arrGenders,
+                gender : arrGenders && arrGenders.length > 0 ? arrGenders[0].key :''
             })
         }
         if (prevProps.roleRedux !== this.props.roleRedux) {
+            let arrRoles = this.props.roleRedux;
             this.setState({
-                roleArr: this.props.roleRedux
+                roleArr: arrRoles,
+                role : arrRoles && arrRoles.length > 0 ? arrRoles[0].key: ''
             })
         }
         if (prevProps.positionRedux !== this.props.positionRedux) {
+            let arrPositions = this.props.positionRedux;
             this.setState({
-                positionArr: this.props.positionRedux
+                positionArr: arrPositions,
+                position: arrPositions && arrPositions.length > 0 ? arrPositions[0].key : ''
             })
         }   
     }
@@ -53,9 +70,9 @@ async componentDidMount() {
         if(file){
             let objectUrl = URL.createObjectURL(file);
             this.setState({
-                previewImgURL: objectUrl
+                previewImgURL: objectUrl,
+                avatar:file,
             })
-            console.log('check img: ', objectUrl);
         }
         
 
@@ -68,6 +85,49 @@ async componentDidMount() {
         })
     }
 
+    handleSaveUser = () =>{
+        let isValid = this.checkValidateInput();
+        if (isValid==false) return;
+        //fire redux actiona
+        this.props.createNewUser({
+            email: this.state.email,
+            password: this.state.password,
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            address: this.state.address,
+            phoneNumber: this.state.phoneNumber,
+            gender: this.state.gender,
+            roleId: this.state.role,
+            positionId: this.state.position
+            
+        })
+        console.log('before submit check state: ', this.state)
+    }
+
+    checkValidateInput = () =>{
+        let isValid = true;
+        let arrCheck = ['email', 'password', 'firstName',
+            'lastName', 'phoneNumber', 'address']
+        
+        for (let i = 0; i < arrCheck.length; i++) {
+            if (!this.state[arrCheck[i]]) {
+                isValid =false;
+                alert('this input is required: '+ arrCheck[i])
+                break;
+            }
+        }
+        return isValid;
+
+    }
+
+    onChangeInput = (event, id)=>{
+        let copyState ={...this.state}
+        copyState[id] = event.target.value;
+        this.setState({
+            ...copyState
+        })
+    }
+
 
     render() {
         
@@ -76,6 +136,12 @@ async componentDidMount() {
         let positions = this.state.positionArr;
         let language = this.props.language;
         let isGetGenders = this.props.isLoadingGender;
+
+        let {email, password, firstName,
+             lastName, phoneNumber, address,
+              gender, position,role,avatar
+        } = this.state;
+
         return (
             <div className='user-redux-container'>
                 <div className='title'>
@@ -89,35 +155,55 @@ async componentDidMount() {
                             <div className="col-12">{isGetGenders === true ? 'Loading genders' : ''}</div>
                             <div className="col-3">
                                 <label><FormattedMessage id="manage-user.email" /></label>
-                                <input className="form-control"type="email" />
+                                <input className="form-control"type="email" 
+                                    value={email}
+                                    onChange={(event)=> {this.onChangeInput(event, 'email')}}
+                                />
                             </div>
                             <div className="col-3">
                                 <label><FormattedMessage id="manage-user.password" /></label>
-                                <input className="form-control"type="password" />
+                                <input className="form-control"type="password" 
+                                    value={password}
+                                    onChange={(event)=> {this.onChangeInput(event, 'password')}}
+                                />
                             </div>
                             <div className="col-3">
                                 <label><FormattedMessage id="manage-user.first-name" /></label>
-                                <input className="form-control" type="text" />
+                                <input className="form-control" type="text" 
+                                    value={firstName}
+                                    onChange={(event)=> {this.onChangeInput(event, 'firstName')}}
+                                />
                             </div>
                             <div className="col-3">
                                 <label><FormattedMessage id="manage-user.last-name" /></label>
-                                <input className="form-control" type="text" />
+                                <input className="form-control" type="text" 
+                                    value={lastName}
+                                    onChange={(event)=> {this.onChangeInput(event, 'lastName')}}
+                                />
                             </div>
                             <div className="col-3">
                                 <label><FormattedMessage id="manage-user.phone-number" /></label>
-                                <input className="form-control" type="text" />
+                                <input className="form-control" type="text" 
+                                    value={phoneNumber}
+                                    onChange={(event)=> {this.onChangeInput(event, 'phoneNumber')}}
+                                />
                             </div>
                             <div className="col-9">
                                 <label><FormattedMessage id="manage-user.address" /></label>
-                                <input className="form-control" type="text" />
+                                <input className="form-control" type="text" 
+                                    value={address}
+                                    onChange={(event)=> {this.onChangeInput(event, 'address')}}
+                                />
                             </div>
                             <div className="col-3">
                                 <label><FormattedMessage id="manage-user.gender" /></label>
-                                <select className="form-control">
+                                <select className="form-control"
+                                    onChange={(event)=> {this.onChangeInput(event, 'gender')}}
+                                >
                                     {genders && genders.length > 0 &&
                                         genders.map((item, index) => {
                                             return (
-                                            <option key = {index} >
+                                            <option key = {index} value={item.key}>
                                             {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
                                             </option>
                                             )
@@ -128,11 +214,13 @@ async componentDidMount() {
                             </div>
                             <div className="col-3">
                                 <label><FormattedMessage id="manage-user.position" /></label>
-                                <select className="form-control">
+                                <select className="form-control"
+                                     onChange={(event)=> {this.onChangeInput(event, 'position')}}
+                                >
                                 {positions && positions.length > 0 
                                     && positions.map((item, index)=>{
                                         return(
-                                            <option key = {index} >
+                                            <option key = {index} value={item.key}>
                                             {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
                                             </option>
                                         );
@@ -141,11 +229,13 @@ async componentDidMount() {
                             </div>
                             <div className="col-3">
                                 <label><FormattedMessage id="manage-user.role" /></label>
-                                <select className="form-control">
+                                <select className="form-control"
+                                     onChange={(event)=> {this.onChangeInput(event, 'role')}}
+                                >
                                     {roles && roles.length > 0 
                                     && roles.map((item, index)=>{
                                         return(
-                                            <option key = {index} >
+                                            <option key = {index} value={item.key}>
                                             {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
                                             </option>
                                         );
@@ -170,7 +260,9 @@ async componentDidMount() {
                                 
                             </div>
                            <div className="col-12 mt-3">
-                           <button className="btn btn-primary"><FormattedMessage id="manage-user.save" /></button>
+                           <button className="btn btn-primary"
+                                onClick={()=> this.handleSaveUser()}
+                           ><FormattedMessage id="manage-user.save" /></button>
                            </div>
                         </div>
                        
@@ -208,6 +300,8 @@ const mapDispatchToProps = dispatch => {
         getPositionStart: () => dispatch(actions.fetchPositionStart()),
 
         getRoleStart: () => dispatch(actions.fetchRoleStart()),
+
+        createNewUser: (data) => dispatch(actions.createNewUser(data))
     };
 };
 
--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -21,7 +21,7 @@ const adminReducer = (state = initialState, action) => {
                 ...state
                 
             }
-        case actionTypes.FETCH_GENDER_FAIDED:
+        case actionTypes.FETCH_GENDER_FAILDED:
             state.isLoadingGender = false;
             state.genders = [];
             return {
@@ -34,7 +34,7 @@ const adminReducer = (state = initialState, action) => {
                 ...state
                     
             }
-        case actionTypes.FETCH_POSITION_FAIDED:
+        case actionTypes.FETCH_POSITION_FAILDED:
             state.positions = [];
             return {
                 ...state,
@@ -46,7 +46,7 @@ const adminReducer = (state = initialState, action) => {
                 ...state
                         
             }
-        case actionTypes.FETCH_ROLE_FAIDED:
+        case actionTypes.FETCH_ROLE_FAILDED:
             state.roles = [];
             return {
                 ...state,
