c37657581a88fe291cf477c8c3ab00932637abc4
Le Cong Nguyen <lenguyentime16@gmail.com>
2023-12-27 16:54:48 +0700

    #87

--- src/containers/Student/Teacher/Modal/BookingModal.js
+++ src/containers/Student/Teacher/Modal/BookingModal.js
@@ -11,6 +11,7 @@ import { LANGUAGES } from '../../../../utils';
 import Select from 'react-select';
 import { postStudentBookAppointment } from '../../../../services/userService';
 import {toast} from "react-toastify"
+import moment from 'moment'
 
 
 class BookingModal extends Component {
@@ -93,8 +94,41 @@ class BookingModal extends Component {
         this.setState({ selectedGender: selectedOption });
     }
 
+    buildTimeBooking = (dataTime) => {
+        let {language} = this.props;
+        if (dataTime && !_.isEmpty(dataTime)) {
+            let time = language === LANGUAGES.VI ?
+                dataTime.timeTypeData.valueVi : dataTime.timeTypeData.valueEn;
+
+                let date = language === LANGUAGES.VI ?
+                moment.unix(+dataTime.date / 1000).format('dddd - DD/MM/YYYY')
+                :
+                moment.unix(+dataTime.date / 1000).locale('en').format('ddd - DD/MM/YYYY')
+
+                return `${time} - ${date}`
+        }
+
+        return ''
+    }
+
+    buildTeacherName = (dataTime) => {
+        let {language} = this.props;
+        if (dataTime && !_.isEmpty(dataTime)) {
+            let name = language === LANGUAGES.VI ?
+            `${dataTime.teacherData.lastName} ${dataTime.teacherData.firstName}`
+            :
+            `${dataTime.teacherData.firstName} ${dataTime.teacherData.lastName}`
+
+            return name
+        }
+        
+        return ''
+    }
+
     handleConfirmBooking = async () => {
         let date = new Date(this.state.birthday).getTime();
+        let timeString = this.buildTimeBooking(this.props.dataTime)
+        let teacherName = this.buildTeacherName(this.props.dataTime)
 
         let res = await postStudentBookAppointment({
             fullName: this.state.fullName,
@@ -105,7 +139,10 @@ class BookingModal extends Component {
             date: date,
             selectedGender: this.state.selectedGender.value,
             teacherId: this.state.teacherId,
-            timeType: this.state.timeType
+            timeType: this.state.timeType,
+            language: this.props.language,
+            timeString: timeString,
+            teacherName: teacherName
         })
 
         if (res && res.errCode === 0) {
